2f0adb9c483e70533eb1e2218f05c8eb7a5f8ba2
Krinesh <pkrinesh95@gmail.com>
2023-12-12 17:01:56 +0530

    feat: uno css

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
 
 export const meta: MetaFunction = () => {
 	return [
@@ -7,35 +8,49 @@ export const meta: MetaFunction = () => {
 	]
 }
 
+export function loader() {
+	return {
+		success: 'ok',
+	}
+}
+
 export default function Index() {
+	const data = useLoaderData<typeof loader>()
+
 	return (
-		<div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-			<h1>Welcome to Remix</h1>
-			<ul>
-				<li>
-					<a
-						target='_blank'
-						href='https://remix.run/tutorials/blog'
-						rel='noreferrer'
-					>
-						15m Quickstart Blog Tutorial
-					</a>
-				</li>
-				<li>
-					<a
-						target='_blank'
-						href='https://remix.run/tutorials/jokes'
-						rel='noreferrer'
-					>
-						Deep Dive Jokes App Tutorial
-					</a>
-				</li>
-				<li>
-					<a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
-						Remix Docs
-					</a>
-				</li>
-			</ul>
+		<div
+			style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}
+			className='min-h-screen flex justify-center items-center bg-gray-900'
+		>
+			<div>
+				<h1>Welcome to Remix</h1>
+				<h1>{data.success}</h1>
+				<ul>
+					<li>
+						<a
+							target='_blank'
+							href='https://remix.run/tutorials/blog'
+							rel='noreferrer'
+						>
+							15m Quickstart Blog Tutorial
+						</a>
+					</li>
+					<li>
+						<a
+							target='_blank'
+							href='https://remix.run/tutorials/jokes'
+							rel='noreferrer'
+						>
+							Deep Dive Jokes App Tutorial
+						</a>
+					</li>
+					<li>
+						<a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+							Remix Docs
+						</a>
+					</li>
+				</ul>
+			</div>
 		</div>
 	)
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { unstable_vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { unstable_vitePlugin as remix } from '@remix-run/dev'
+import UnoCSS from 'unocss/vite'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-	plugins: [remix(), tsconfigPaths()],
-});
+	plugins: [remix(), tsconfigPaths(), UnoCSS()],
+})
