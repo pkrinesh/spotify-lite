9282d3098b9846bed8f4409957b0721c056d1f84
Krinesh <pkrinesh95@gmail.com>
2023-12-14 16:05:04 +0530

    feat: zod and ofetch

--- app/server/api/getMe.server.ts
+++ app/server/api/getMe.server.ts
@@ -1,13 +1,35 @@
+import { ofetch } from 'ofetch'
+import { z } from 'zod'
 import { getAuthToken } from '../utils/auth.server'
+
+const MeSchema = z.object({
+	display_name: z.string(),
+	external_urls: z.object({ spotify: z.string() }),
+	href: z.string(),
+	id: z.string(),
+	images: z.array(z.unknown()),
+	type: z.enum(['user']),
+	uri: z.string(),
+	followers: z.object({ href: z.null(), total: z.number() }),
+	country: z.string(),
+	product: z.enum(['free']),
+	explicit_content: z.object({
+		filter_enabled: z.boolean(),
+		filter_locked: z.boolean(),
+	}),
+	email: z.string(),
+})
+
+type Me = z.infer<typeof MeSchema>
 
 export async function getMe(request: Request) {
 	const token = await getAuthToken(request)
-	const res = await fetch('https://api.spotify.com/v1/me', {
+	const res = await ofetch<Me>('https://api.spotify.com/v1/me', {
 		headers: {
 			Authorization: `Bearer ${token}`,
 			'Content-Type': 'application/json',
 		},
 	})
 
-	return await res.json()
+	return res
 }
